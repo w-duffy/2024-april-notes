d00178ffb8519341be7a54221cf0200602e877e4
will <w.duffy@outlook.com>
2024-07-25 16:37:38 -0500

    added decorator and class lecture examples

--- 17-week/4-day/lecture.py
+++ 17-week/4-day/lecture.py
@@ -1,19 +1,18 @@
-
 # Decorators in Python
 
 # 1. Functions are first-class objects in Python.
 # This means they can be passed around and used as arguments.
 
+
 def greet(name):
     return f"Alright, welcome back in, let's wait until {name} rolls in."
-
 
 
 def wrap_greet(funcy_func, name):
     return funcy_func(name)
 
-# print(wrap_greet(greet, "Will"))
 
+# print(wrap_greet(greet, "Will"))
 
 
 # Callbacks
@@ -24,36 +23,81 @@ and print hi if it's will or who are you if it's not
 """
 
 
-def wrapper_func(func):
-    def nested_func(name):
+def outer_func(func):
+    def wrapper(name):
         if name == "will":
+            print("in wrapper", func)
             return func(name)
         return "you're not will"
-    return nested_func
+
+    return wrapper
 
 
+# def say_hello(name):
+#     return f"hi {name}"
+# a_nested_func = outer_func(say_hello)
+# print(a_nested_func("will"))
+
+
+@outer_func
 def say_hello(name):
-    # print("hello world")
     return f"hi {name}"
 
-a_nested_func = wrapper_func(say_hello)
-print(a_nested_func("bri"))
+
+# print(say_hello("will"))
+
+# @outer_func
+# def say_hello_bri(name):
+#     return name
+
+# print(say_hello_bri("bri"))
 
 
+# print(say_hello("will"))
 
+## Let's use our wrapper as a decorator🚀🚀
+
+
+"""
+Pseudo real world exam
+"""
+
+
+def auth_check(route_function):
+    def wrapper(req):
+        if req["is_logged_in"]:
+            route_function(req)
+            return
+        print("You're being redirected. Please log in")
+
+    return wrapper
+
+
+@auth_check
+def create_spot(req):
+    print(f"{req['spot_name']} has been added to the DB")
+
+
+request = {"spot_name": "Over the water bungalo", "is_logged_in": True}
+
+# create_spot(request)
 
 
 # 2. Python supports higher-order functions.
 # These are functions that take other functions as arguments and/or return functions.
 
+
 def make_pretty(func):
     def inner():
         print("I got decorated")
         func()
+
     return inner
+
 
 def ordinary():
     print("I am ordinary")
+
 
 # Decorating `ordinary` manually without `@`
 # pretty = make_pretty(ordinary)
@@ -64,43 +108,47 @@ def ordinary():
 # 3. Using the @ symbol for decorators.
 # The @ symbol offers a shorthand to decorate a function.
 
+
 @make_pretty
 def ordinary():
     print("I am ordinary again")
+
 
 # ordinary()
 # print()
 
 
+# # 4. A more detailed example of a decorator
+# # This decorator prints statements before and after the decorated function is executed.
+
+# # def my_decorator(func):
+# #     def wrapper():
+# #         print("Something is happening before the function is called.")
+# #         func()
+# #         print("Something is happening after the function is called.")
+# #     return wrapper
+
+# # @my_decorator
+# # def say_hello():
+# #     print("Hello!")
+
+# # say_hello()
+# # print()
 
 
+# # 5. Decorators that can accept arguments.
+# # This allows us to decorate functions that accept arguments.
 
+# # def my_func(name):
+# #     print("cool")
 
+# # my_func("will" ,"bri", "sally", "tumr", my_name="will")
 
+# num1 = 1,
+# print(num1)
 
+# num1, num2  = 1, 2
 
-
-
-# 4. A more detailed example of a decorator
-# This decorator prints statements before and after the decorated function is executed.
-
-# def my_decorator(func):
-#     def wrapper():
-#         print("Something is happening before the function is called.")
-#         func()
-#         print("Something is happening after the function is called.")
-#     return wrapper
-
-# @my_decorator
-# def say_hello():
-#     print("Hello!")
-
-# say_hello()
-# print()
-
-
-# 5. Decorators that can accept arguments.
-# This allows us to decorate functions that accept arguments.
 
 # def my_decorator_with_args(func):
 #     def wrapper(*args, **kwargs):
@@ -111,13 +159,107 @@ def ordinary():
 
 # @my_decorator_with_args
 # def greet_with_name(name):
+
 #     print(f"Hello, {name}!")
 
 # greet_with_name("Alice")
-# print()
+
 
 # @my_decorator_with_args
-# def custom_greet(name, greeting="Hello"):
-#     print(f"{greeting}, {name}!")
+# def custom_greet(name, greeting="Hello", phrase="niiice"):
+#     print(f"{greeting}, {name}! {phrase}")
 
 # custom_greet("Bob", greeting="Good morning")
+
+# dict1  = {"hello": "world"}
+# print(dict1["hello"])
+
+# name = "hello"
+# print(dict1.name)
+# print(dict1["hello"])
+
+
+# class Dog():
+#     def __init__(self, name, my_woof):
+#         self.name = name
+#         self.my_woof = my_woof
+#         self.is_cute = True
+
+#     def __repr__(self):
+#         return f"{self.name} said {self.my_woof}"
+
+
+# fido = Dog("fido")
+
+# # print("down here", fido.name)
+# print(fido)
+
+
+class Dragon:
+    def __init__(self, name, color, is_good=True):
+        self._name = name
+        self._color = color
+        self._is_good = is_good
+
+    def breathes_fire(self):
+        return f"{self._name} breathes fire everywhere! BURN!!!"
+
+    def change_nature(self):
+        self._is_good = not self._is_good
+        if self._is_good == True:
+            return f"{self._name} is a good dragon!"
+        else:
+            return f"{self._name} is a bad dragon!"
+
+    def to_dict(self):
+        return{
+            "name": self._name,
+            "color": self._color,
+            "is_good": self._is_good
+        }
+
+puff = Dragon("puff", "blue")
+
+# print(puff.to_dict())
+# print(puff.change_nature())
+# print(puff.to_dict())
+
+
+
+
+# SCREAMING_CASE = "don't change this"
+
+
+class Animal():
+    pass
+
+class Cat(Animal):
+    pass
+
+class User():
+    def __init__(self, name, password, dog="lucky"):
+        self._name = name
+        self._password = password
+        self._dog = dog
+
+    @property
+    def my_password(self):
+        if True: # intense hashing algo
+            self._dog = "Lucky"
+
+    @my_password.setter
+    def my_password(self, new_password):
+        self._password = new_password
+        return "updated!"
+
+    def to_dict(self):
+        return{
+            "name": self._name,
+            "password": self._password,
+            "dog": self._dog
+        }
+
+will = User("will", "12345")
+
+
+print(will.to_dict())
--- 17-week/4-day/notes.py
+++ 17-week/4-day/notes.py
@@ -1,13 +1,49 @@
-def name_check(fn):
-    def instructor_name(name):
-        if name  == "will":
-            return "hi Will"
-        return fn(name)
-    return instructor_name
+## Getter
+class Icon():
+    def __init__(self, color, shape):
+        self.color = color
+        self.shape = shape
 
-def what_is_your_name(name):
-    return name
+    # getter for ~secret~ password
+    @property
+    def my_password(self):
+        return "somebody's secret password"
 
-res = name_check(what_is_your_name)
-print(res("will"))
+my_icon = Icon("blue", "square")
+print(my_icon.color)
+
+# call the getter method as if we were just
+# reading a property
+print(my_icon.my_password)
+
+
+
+## Setter
+class Icon():
+    def __init__(self, color, shape, pswd):
+        self.color = color
+        self.shape = shape
+
+        # set initial ~secret~ password
+        # this calls the setter method!
+        self.my_password = pswd
+
+    # getter for ~secret~ password
+    @property
+    def my_password(self):
+        return self._password
+
+    # setter for ~secret~ password
+    @my_password.setter
+    def my_password(self, new_val):
+        print("hashing password....")
+        self._password =  str(new_val) + "12345" * 3
+
+my_icon = Icon("blue", "square", "beepboop")
+print(my_icon.my_password)
+
+# call the setter method as if we were
+# setting my_password as a regular property
+my_icon.my_password = "new thing"
+print(my_icon.my_password)
 
