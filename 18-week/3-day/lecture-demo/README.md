3470be33c30afee05e09a04a64e8408894dd32af
will <w.duffy@outlook.com>
2024-07-31 13:18:38 -0500

    added lecture code

---
+++ 18-week/3-day/lecture-demo/README.md
@@ -0,0 +1,7 @@
+# Creating Tables and Seeders
+
+This app simply creates tables, and seeds the db.
+
+run the below to replicate:
+
+pipenv run python seeders.py
---
+++ 18-week/3-day/lecture-demo/app/__init__.py
@@ -0,0 +1,65 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
+from .models import db, User
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+db.init_app(app)
+
+
+
+# READ all
+@app.route("/")
+def index():
+    users = User.query.all()
+    print("GET ALL", users)
+    return [user.to_dict() for user in users]
+
+
+
+# READ one
+@app.route("/<int:id>")
+def get_user(id):
+    user = User.query.get(id)
+    return_dict = {}
+    return_dict["name"] = user.name
+    return user.to_dict()
+
+
+
+
+# CREATE new
+@app.route("/new")
+def create_user():
+    new_user = User({"name": "will2"})
+    print(new_user)
+    db.session.add(new_user)
+    db.session.commit()
+    return new_user.to_dict()
+
+
+
+
+@app.route("/update/<int:id>")
+def update_user(id):
+    user = User.query.get(id)
+    user.name = "new name"
+    db.session.commit()
+    return user.to_dict()
+
+@app.route("/delete/<int:id>")
+def delete_user(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return "Success!"
+
+
+@app.route("/test")
+def testing():
+    users = User.query.all()
+    print(users)
+    return [user.to_dict() for user in users]
